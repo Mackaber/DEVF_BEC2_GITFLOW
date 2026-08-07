aef0358a79af127c6912fd4db0f3423ad7dcc0e9
Mackaber <mkbravo555@gmail.com>
2026-08-06 19:14:16 -0600

    comandos de control de versiones

--- comandos.sh
+++ comandos.sh
@@ -1,8 +1,10 @@
+# Comandos de control de versiones
+
 git init # Inicializa el repositorio
 git status # Nos indica el estatus del repositorio
 git log # Historial de todos los cambios
 
 git add # Mover los archivos al apartado de staging
 git commit -m "Mensaje del commit" # Confirma los cambios (los mueve al apartado de committed)
-git push
-git pull
+git push # Manda los cambios al repositorio remoto
+git pull # Obtiene los últimos cambios del repositorio remoto

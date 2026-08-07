7951ce0ace45765215745c56287583af78d8a04b
Mackaber <mkbravo555@gmail.com>
2026-08-06 19:11:16 -0600

    Commit inicial

---
+++ comandos.sh
@@ -0,0 +1,8 @@
+git init # Inicializa el repositorio
+git status # Nos indica el estatus del repositorio
+git log # Historial de todos los cambios
+
+git add # Mover los archivos al apartado de staging
+git commit -m "Mensaje del commit" # Confirma los cambios (los mueve al apartado de committed)
+git push
+git pull
